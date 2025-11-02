2548f70038a85405b1b4e29900c4743f942ec069
Andrey <andrey-bakhvalov@mail.ru>
2025-11-02 11:07:09 +0000

    is_same added

--- main.cpp
+++ main.cpp
@@ -76,3 +76,19 @@ public:
     ~ScopePtr() { delete [] m_ptr; }
 };
 
+//is_same
+
+template<typename T, typename U>
+struct is_same {
+    static constexpr bool value = false;
+};
+
+template<typename T>
+struct is_same<T, T> {
+    static constexpr bool value = true;
+};
+
+//are_same
+
+
+
